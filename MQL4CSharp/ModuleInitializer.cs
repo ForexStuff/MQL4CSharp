0ab8cb4170149469a2e90c1c4e1ff71526b7ed9d
Martin van Beurden <chadoe@gmail.com>
2016-12-03 23:00:35 +0100

    Enable loading dll's from library
    Inserts a module initializer which enabled loading dll's from the main dll location so MQL4CSharp.dll and any dependency can be placed in the userprofile library folder instead of the main terminal.exe directory.

---
+++ MQL4CSharp/ModuleInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+/// <summary>
+/// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.
+/// </summary>
+public static class ModuleInitializer
+{
+    /// <summary>
+    /// Initializes the module.
+    /// </summary>
+    public static void Initialize()
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += delegate (object sender, ResolveEventArgs args)
+        {
+            string assemblyFile = (args.Name.Contains(','))
+                ? args.Name.Substring(0, args.Name.IndexOf(','))
+                : args.Name;
+
+            assemblyFile += ".dll";
+
+            string absoluteFolder = new FileInfo((new Uri(Assembly.GetExecutingAssembly().CodeBase)).LocalPath).Directory.FullName;
+            string targetPath = Path.Combine(absoluteFolder, assemblyFile);
+
+            try
+            {
+                return Assembly.LoadFile(targetPath);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        };
+    }
+}
